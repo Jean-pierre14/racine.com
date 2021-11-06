39565ce5507b9359a7f7db0bbcd0de8dec3ba8c4
User <chiruzabisimwa@gmail.com>
2021-11-06 10:56:46 +0200

    UI fix

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,8 @@
 let output = '';
 const paragraph = document.getElementById('Datas');
-paragraph.innerHTML = 'Loading...';
+if (paragraph) {
+  paragraph.innerHTML = 'Loading...';
+}
 
 fetch('https://jsonplaceholder.typicode.com/todos')
   .then(response => response.json())
@@ -29,3 +31,30 @@ fetch('https://jsonplaceholder.typicode.com/todos')
       console.log('No data')
     }
   })
+
+// Fetch users using the fake jsonplaceholder api
+const UsersDom = document.getElementById('Datas-users')
+let data = 'Loading'
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then(res => res.json())
+  .then(json => {
+    if (json.length > 0) {
+      data = ''
+      json.forEach((user) => {
+        data += `
+        <li>
+          <a href="#user=uniqueId">
+              <span>
+                  <img src="./assets/img/racine14.jpg" alt="Friends img" class="img-profil" />
+              </span>
+              <span>${user.name}</span>
+          </a>
+      </li>
+        `
+      })
+    } else {
+      data = '<p class="info danger">There no data</p>'
+    }
+    UsersDom.innerHTML = data
+  })
+  .catch((e) => console.log(e))
