7ddaa9dd676422ec5c1dd172e251f4fda6f65a10
Jean-pierre14 <chiruzabisimwa@gmail.com>
2021-12-11 13:58:52 +0100

    Server Setup

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
 const exp = require('express'), {success, error} = require('consola')
+
+const app = exp(), PORT = process.env.PORT || 7000
+
+app.get('/', (req, res)=>{
+    res.json("Hello")
+})
+app.listen(PORT, (e)=>{
+    if(e) error({message: `${e}`, badge: true})
+    success({message: `Server run on port ${PORT}`})
+})
