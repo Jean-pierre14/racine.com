5c0671d541e8cc2a5c0e6b90a0f7250c3e7442fd
Jean-pierre14 <chiruzabisimwa@gmail.com>
2022-01-03 10:17:02 +0100

    Html2Pdf is not good as i think

--- assets/js/pdf.js
+++ assets/js/pdf.js
@@ -1,5 +1,13 @@
-window.onload = () =>{
-    document.getElementById('Download').addEventListener('click', ()=>{
-        alert('Clicked')
+window.onload = () => {
+    document.getElementById('Download').addEventListener('click', () => {
+        const invoice = this.document.getElementById('profil')
+        var opt = {
+            margin: 0.5,
+            filename: 'mr Bisimwa.pdf',
+            image: { type: 'jpeg', quality: 0.98 },
+            html2canvas: { scale: 3 },
+            jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
+        };
+        html2pdf().from(invoice).set(opt).save()
     })
 }
