814a1d58dd8b62345b6ec9f57a224b7b57cf2c1a
Jean-pierre14 <chiruzabisimwa@gmail.com>
2023-03-21 09:01:43 +0200

    Styling

--- views/src/App.js
+++ views/src/App.js
@@ -2,12 +2,15 @@ import Footer from "./components/global/Footer";
 import Header from "./components/global/Header";
 import Main from "./components/global/Main";
 
+import "./App.css";
 const App = () => {
   return (
     <>
-      <Header />
-      <Main />
-      <Footer />
+      <div className="body">
+        <Header />
+        <Main />
+        <Footer />
+      </div>
     </>
   );
 };
--- views/src/components/global/Header.jsx
+++ views/src/components/global/Header.jsx
@@ -1,5 +1,19 @@
+import "./Header.css";
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="container header">
+      <nav className="nav">
+        <div className="logo">Logo</div>
+        <div className="navLinks">
+          <ul>
+            <li>Home</li>
+            <li>Profil</li>
+            <li>About Me</li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
 };
 
 export default Header;
