9ea39157b976e2d4bf9226ef0ec012a2237b8757
Jean-pierre14 <chiruzabisimwa@gmail.com>
2023-04-08 18:21:51 +0200

    React init

---
+++ views/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/global/Header";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <Header />
+      </div>
+    </>
+  );
+}
+
+export default App;
--- views/src/components/global/Header.jsx
+++ views/src/components/global/Header.jsx
@@ -1,19 +1,7 @@
 import "./Header.css";
+
 const Header = () => {
-  return (
-    <div className="container header">
-      <nav className="nav">
-        <div className="logo">Logo</div>
-        <div className="navLinks">
-          <ul>
-            <li>Home</li>
-            <li>Profil</li>
-            <li>About Me</li>
-          </ul>
-        </div>
-      </nav>
-    </div>
-  );
+  return <div>Header</div>;
 };
 
 export default Header;
