077c6323ec9c63c462900123539e17e61000883f
Jean-pierre14 <chiruzabisimwa@gmail.com>
2023-06-21 07:28:21 +0300

    add: views config and get start

---
+++ controllers/index.controllers.js
@@ -0,0 +1,15 @@
+export const postOne = async (res, res) => {
+  await res.json("Post data");
+};
+export const getAll = async (res, res) => {
+  await res.json("Get all");
+};
+export const getOne = async (res, res) => {
+  await res.json("Get one");
+};
+export const updateOne = async (res, res) => {
+  await res.json("update");
+};
+export const deleteOne = async (res, res) => {
+  await res.json("Delete one");
+};
--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config({ path: ".env" });
 
 const app = express(),
   PORT = process.env.PORT || 5001;
 
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Engine
+app.set("view engine", "ejs");
+
+// Static file
+app.use("/assets", express.static("public"));
+
 app.get("/", (req, res) => {
-  res.json({ message: "Racine" });
+  res.render("index");
 });
 
 const Server = () => {
